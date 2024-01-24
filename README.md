6ad6e9726acc9371b0b63a3d2f922e424eaa925a
mariz <marrise@oostvoort.com>
2024-01-24 15:28:16 +0800

    feat: add deploy script

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # PixeLAW Deployer
 Auto deploys approved apps in a world
+
+## Approved Apps
+A JSON file that contains an array of apps that will be deployed to the world. The app must be using the correct
+version of the PixeLAW core (otherwise it will be ignored by the auto deployer).
+
+### Adding an app
+Make a pull request to this repository, and follow the following format to add your app into the approvedApps.json.
+````json
+{
+  "name": "put the name of your app here",
+  "git-repository": "add the git repo url here",
+  "contracts-directory": "path to your contracts directory relative to the root folder",
+  "scripts": [
+    "list the custom scripts needed to initialize your app", 
+    "do not include initialize and upload_manifest"
+  ]
+}
+````
+
+## Running the deployer
+To run the deployer, simply run the following:
+````shell
+bash deploy.sh
+````
---
+++ deploy.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Load variables from .env file
+source .env
+
+# Parse JSON using jq tool
+json=$(cat approvedApps.json)
+
+length=$(echo ${json} | jq '. | length')
+
+# Check if RPC_URL environment variable exists
+if [ -z "${RPC_URL}" ]; then
+  # If not, set it
+  RPC_URL="https://api.cartridge.gg/x/${WORLD_NAME}/katana"
+fi
+
+if [ ! -d "dist" ]; then
+  # If not, create it
+  mkdir dist
+fi
+
+
+# make directory dist if it does not exist and all apps cloned there should be inside that directory
+cd dist
+
+dist_dir=$(pwd)
+
+echo "----------------------------------------------------------------------------"
+echo "Deploying ${length} apps"
+
+for (( i=0; i<${length}; i++ ))
+do
+  app=$(echo ${json} | jq -r ".[${i}]")
+  name=$(echo ${app} | jq -r ".name")
+  git_repo=$(echo ${app} | jq -r '."git-repository"')
+  contracts_dir=$(echo ${app} | jq -r '."contracts-directory"')
+  scripts=$(echo ${app} | jq -r ".scripts[]")
+
+  echo "----------------------------------------------------------------------------"
+  echo "[${i}] App: ${name}"
+
+  echo "Cloning repository (${git_repo}) to ${i}"
+  git clone ${git_repo} ${i}
+
+  cd ${i}/${contracts_dir}
+
+  echo "Building contract"
+  sozo build
+
+  sed -i 's#rpc_url = ".*"#rpc_url = "'"$RPC_URL"'"#' Scarb.toml
+  sed -i 's/account_address = ".*"/account_address = "'"$ACCOUNT_ADDRESS"'"/' Scarb.toml
+  sed -i 's/private_key = ".*"/private_key = "'"$PRIVATE_KEY"'"/' Scarb.toml
+
+
+  echo "Deploying the contract"
+
+  sozo migrate --name $WORLD_NAME
+
+  scarb run initialize
+  scarb run upload_manifest $MANIFEST_URL
+
+  for script in ${scripts}
+  do
+    echo "scarb run ${script}"
+    scarb run ${script}
+    sleep 10
+  done
+
+  cd dist_dir
+
+done
+
+echo "----------------------------------------------------------------------------"
+echo "Completed deployment successfully"
+echo "----------------------------------------------------------------------------"
